f2113d1f3e1e3bf98734cad944f893b204bdf03c
jeremias256 <jere.menacho@gmail.com>
2022-10-27 10:26:58 -0300

    fragment

--- Sección-4/src/FirtsApp.jsx
+++ Sección-4/src/FirtsApp.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 export const FirtsApp = () => {
-    {/*37 react debe retornar solo un bloque */}
+    {/*37 react debe retornar solo un nodo padre */}
     return (
-        <h1>Jeremias</h1>
-        
+        <>    
+            <h1>Jeremias</h1>
+            <h2>Desarrollador</h2>
+        </>    
     )
 }
